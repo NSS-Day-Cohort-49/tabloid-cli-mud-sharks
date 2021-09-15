7dd9392c5198828350684107affde9d0ec5ff17e
dparsley1203 <dparsley1203@gmail.com>
2021-09-15 15:46:20 -0500

    merged

--- TabloidCLI/Repositories/BlogRepository.cs
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -61,6 +61,21 @@ namespace TabloidCLI.Repositories
             }
         }
 
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE from Blog WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
 
     }
 }
