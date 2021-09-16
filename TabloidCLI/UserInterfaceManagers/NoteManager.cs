380adabb855f7dc40a46ee4b8d1aa05b92cb226e
averynoble <averynoble.an@gmail.com>
2021-09-16 14:38:06 -0500

    Creating NoteManager which allows for menu on post details page

---
+++ TabloidCLI/UserInterfaceManagers/NoteManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class NoteManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private PostRepository _postRepository;
+        private BlogRepository _blogRepository;
+        private NoteRepository _noteRepository;
+        private string _connectionString;
+
+        public NoteManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _connectionString = connectionString;
+            _blogRepository = new BlogRepository(connectionString);
+            _noteRepository = new NoteRepository(connectionString);
+        }
+    }
+}
